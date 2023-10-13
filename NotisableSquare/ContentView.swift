7266ef637a0c0c44f3d7a5da89285f45d85c6911
lord-of-the-lost <ignatov.99@yandex.ru>
2023-10-13 18:59:54 +0300

    add square with blendMode

--- NotisableSquare/ContentView.swift
+++ NotisableSquare/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @GestureState private var dragOffset = CGSize.zero
+    @State private var position = CGPoint(x: 0, y: 0)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(spacing: 0) {
+                Color.white
+                Color.pink
+                Color.yellow
+                Color.black
+            }
+            .ignoresSafeArea()
+    
+            Rectangle()
+                .foregroundColor(.white)
+                .blendMode(.difference)
+                .overlay(.blendMode(.hue))
+                .overlay(Color.white.blendMode(.overlay))
+                .overlay(Color.black.blendMode(.overlay))
+                .frame(width: 100, height: 100)
+                .cornerRadius(10)
+                .offset(x: position.x + dragOffset.width, y: position.y + dragOffset.height)
+                .gesture(
+                    DragGesture()
+                        .updating($dragOffset) { value, dragState, _ in
+                            dragState = value.translation
+                        }
+                        .onEnded { value in
+                            self.position.x += value.translation.width
+                            self.position.y += value.translation.height
+                        })
         }
-        .padding()
     }
 }
 
